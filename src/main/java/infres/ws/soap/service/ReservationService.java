948ebf08c922d61ce54bd0c87e75ee0ba3ada8d2
Samuel Ohayon <samuelohayon0411@gmail.com>
2023-06-19 17:53:28 +0200

    Modify ReservationService

--- src/main/java/infres/ws/soap/service/ReservationService.java
+++ src/main/java/infres/ws/soap/service/ReservationService.java
@@ -9,4 +9,21 @@ import java.util.List;
 
 public class ReservationService {
     public List<Reservation> reservations = new ArrayList<Reservation>();
+    public List<Room> rooms = new ArrayList<Room>();
+    public Room getRoom(Integer roomId) throws Exception {
+        return rooms.stream()
+                .filter(room -> room.getRoomNumber() == roomId)
+                .findFirst()
+                .orElseThrow(() -> new Exception("Room not found"));
+    }
+    public Reservation createReservation(Date StartDate, Date EndDate, int idRoom, String customerName) throws Exception {
+        Reservation reservation = new Reservation();
+        reservation.setStartDate(StartDate);
+        reservation.setEndDate(EndDate);
+        Room room = getRoom(idRoom);
+        reservation.setRoom(room);
+        reservation.setCustomerName(customerName);
+        return reservation;
+    }
+
 }
--- src/main/java/infres/ws/soap/service/RoomService.java
+++ src/main/java/infres/ws/soap/service/RoomService.java
@@ -16,4 +16,5 @@ public class RoomService {
     public List<Room> getAllRoom(){
         return rooms;
     }
+
 }
