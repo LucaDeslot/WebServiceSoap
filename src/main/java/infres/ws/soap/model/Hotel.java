4014238dc279ceb6c84beaa17ea0071144d7eb91
Luca <deslot.luca@gmail.com>
2023-06-19 11:48:06 +0200

    Model implemented

--- src/main/java/infres/ws/soap/model/Hotel.java
+++ src/main/java/infres/ws/soap/model/Hotel.java
@@ -1,8 +1,43 @@
 package infres.ws.soap.model;
 
+import java.util.ArrayList;
+
 public class Hotel {
 
+    private int id;
     private String name;
+    private String adress;
+    private ArrayList<Room> rooms;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public ArrayList<Room> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(ArrayList<Room> rooms) {
+        this.rooms = rooms;
+    }
 }
--- src/main/java/infres/ws/soap/model/Reservation.java
+++ src/main/java/infres/ws/soap/model/Reservation.java
@@ -1,4 +1,54 @@
 package infres.ws.soap.model;
 
+import java.util.Date;
+
 public class Reservation {
+
+    private int id; // Un identifiant unique pour la réservation
+    private Room room; // La chambre réservée
+    private Date startDate; // La date de début de la réservation
+    private Date endDate; // La date de fin de la réservation
+    private String customerName; // Le nom du client qui a fait la réservation
+
+    // Getters et Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
 }
