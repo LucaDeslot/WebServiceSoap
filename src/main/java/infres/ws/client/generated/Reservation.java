4169585a0fb86d101afc75bf059d95f61ea21da2
Luca <deslot.luca@gmail.com>
2023-06-20 11:09:04 +0200

    Add reservation generated services

---
+++ src/main/java/infres/ws/client/generated/Reservation.java
@@ -0,0 +1,164 @@
+
+package infres.ws.client.generated;
+
+import javax.xml.datatype.XMLGregorianCalendar;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlSchemaType;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour reservation complex type.
+ * 
+ * <p>Le fragment de sch�ma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * <pre>{@code
+ * <complexType name="reservation">
+ *   <complexContent>
+ *     <restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       <sequence>
+ *         <element name="customerName" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         <element name="endDate" type="{http://www.w3.org/2001/XMLSchema}dateTime" minOccurs="0"/>
+ *         <element name="id" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         <element name="room" type="{http://endpoint.soap.ws.infres/}room" minOccurs="0"/>
+ *         <element name="startDate" type="{http://www.w3.org/2001/XMLSchema}dateTime" minOccurs="0"/>
+ *       </sequence>
+ *     </restriction>
+ *   </complexContent>
+ * </complexType>
+ * }</pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "reservation", propOrder = {
+    "customerName",
+    "endDate",
+    "id",
+    "room",
+    "startDate"
+})
+public class Reservation {
+
+    protected String customerName;
+    @XmlSchemaType(name = "dateTime")
+    protected XMLGregorianCalendar endDate;
+    protected int id;
+    protected Room room;
+    @XmlSchemaType(name = "dateTime")
+    protected XMLGregorianCalendar startDate;
+
+    /**
+     * Obtient la valeur de la propri�t� customerName.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    /**
+     * D�finit la valeur de la propri�t� customerName.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCustomerName(String value) {
+        this.customerName = value;
+    }
+
+    /**
+     * Obtient la valeur de la propri�t� endDate.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getEndDate() {
+        return endDate;
+    }
+
+    /**
+     * D�finit la valeur de la propri�t� endDate.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setEndDate(XMLGregorianCalendar value) {
+        this.endDate = value;
+    }
+
+    /**
+     * Obtient la valeur de la propri�t� id.
+     * 
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * D�finit la valeur de la propri�t� id.
+     * 
+     */
+    public void setId(int value) {
+        this.id = value;
+    }
+
+    /**
+     * Obtient la valeur de la propri�t� room.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Room }
+     *     
+     */
+    public Room getRoom() {
+        return room;
+    }
+
+    /**
+     * D�finit la valeur de la propri�t� room.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Room }
+     *     
+     */
+    public void setRoom(Room value) {
+        this.room = value;
+    }
+
+    /**
+     * Obtient la valeur de la propri�t� startDate.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getStartDate() {
+        return startDate;
+    }
+
+    /**
+     * D�finit la valeur de la propri�t� startDate.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setStartDate(XMLGregorianCalendar value) {
+        this.startDate = value;
+    }
+
+}
