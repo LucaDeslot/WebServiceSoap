96aba75ddb4b61a6bd2b784de433da8846769884
Samuel Ohayon <samuelohayon0411@gmail.com>
2023-06-20 16:59:24 +0200

    Change type Room

--- src/main/java/infres/ws/soap/service/RoomService.java
+++ src/main/java/infres/ws/soap/service/RoomService.java
@@ -7,34 +7,34 @@ import java.util.Arrays;
 import java.util.List;
 
 public class RoomService {
-    private Room[] rooms;
+    private final Room[] rooms = new Room[10];
     public RoomService() {
         // Créer des chambres et les ajouter à la liste
         Room basicRoom = new Room();
         basicRoom.setRoomNumber(1);
         basicRoom.setType("Basique");
         basicRoom.setPrice(50.0);
-        rooms[1]=basicRoom;
+        rooms[0]=basicRoom;
 
         Room middleRoom = new Room();
         middleRoom.setRoomNumber(2);
         middleRoom.setType("Middle Gamme");
         middleRoom.setPrice(100.0);
-        rooms[2]=middleRoom;
+        rooms[1]=middleRoom;
 
         Room highEndRoom = new Room();
         highEndRoom.setRoomNumber(3);
         highEndRoom.setType("Haut de Gamme");
         highEndRoom.setPrice(200.0);
-        rooms[3]=highEndRoom;
+        rooms[2]=highEndRoom;
     }
     public Room getRoom(Integer roomId) throws Exception {
-        return Arrays.stream(rooms)
+        return Arrays.stream(this.rooms)
                 .filter(room -> room.getRoomNumber() == roomId)
                 .findFirst()
                 .orElseThrow(() -> new Exception("Room not found"));
     }
     public Room[] getRooms(){
-        return rooms;
+        return this.rooms;
     }
 }
